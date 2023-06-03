043700c09ccf58b827cf9002c7a0c19ac48a54fa
saad el-haziti <88976189+saadelhaziti@users.noreply.github.com>
2023-06-03 11:47:29 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 #python manage.py collectstatic
+pip install whitenoise 
 python manage.py makemigrations
 python manage.py migrate
 pip install django-cors-headers
